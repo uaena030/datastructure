32dd8a3a095ffd275c3dd719fcb82c46b68938ee
uaena <112924804+uaena030@users.noreply.github.com>
2023-10-02 03:07:17 +0800

    11

--- bfs.c
+++ bfs.c
@@ -306,7 +306,7 @@ int short_path(int *array, int start_x, int start_y, int map_size, int end_x, in
 }
 int main()
 {
-    int map_size, i, j, sum = 0;
+    int map_size, i, j;
     int ball_1[2][2], ball_2[2][2];
     scanf("%d", &map_size);
     int m_array[map_size][map_size];
@@ -321,17 +321,15 @@ int main()
     scanf("%d %d %d %d", &ball_1[0][0], &ball_1[0][1], &ball_2[0][0], &ball_2[0][1]);
     scanf("%d %d %d %d", &ball_1[1][0], &ball_1[1][1], &ball_2[1][0], &ball_2[1][1]);
     m_array[ball_1[1][0]][ball_1[1][1]] = 3;
-    int temp = short_path((int *)m_array, ball_1[0][0], ball_1[0][1], map_size, ball_1[1][0], ball_1[1][1]);
-    sum += temp;
+    short_path((int *)m_array, ball_1[0][0], ball_1[0][1], map_size, ball_1[1][0], ball_1[1][1]);
     /*for(int j = 0; j < sum; j++){
         printf("%d", walker[j]);
     }*/
     //printf("%d", sum);
     printf("\n");
-    sum = 0;
     m_array[ball_1[1][0]][ball_1[1][1]] = 0;
     m_array[ball_2[1][0]][ball_2[1][1]] = 3;
-    sum += short_path((int *)m_array, ball_2[0][0], ball_2[0][1], map_size, ball_2[1][0], ball_2[1][1]);
+    short_path((int *)m_array, ball_2[0][0], ball_2[0][1], map_size, ball_2[1][0], ball_2[1][1]);
     /*for (int j = 0; j < sum; j++){
         printf("%d", walker[j]);
     }*/
