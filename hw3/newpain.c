7a1a9a5bb83802c2bc280aec5cd9ad01f9a8d70b
uaena <112924804+uaena030@users.noreply.github.com>
2023-12-23 00:26:15 +0800

    hw3

--- hw3/newpain.c
+++ hw3/newpain.c
@@ -20,41 +20,30 @@ void deleteTree(tree* curNode){
     free(curNode);
 }
 int* BFS(int src, int end, int **maze, int size){
-    int **distance = malloc(size * sizeof(int *));
-    for (int i = 0; i < size; i++)
-        distance[i] = malloc(size * sizeof(int));
-    for (int i = 0; i < size; i++)// i is starting point and j is ending point
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if (maze[i][j] == false)
-                distance[i][j] = -2; // use -2 to represent no linked
-            else
-                distance[i][j] = -1; // -1 to represent linked
-        }
-    }
-    distance[src][src] = 0;// starting point
-    distance[end][end] = -1;// ending point
-
     node queue[size * size + 1];
     int head = 0, tail = 1;
     queue[0].x = src;
     queue[0].y = src;//(src, src) is starting point
     int node_x, node_y;
+    int visited[size], ancient[1000000];
+    for(int i = 0; i < size; i++){
+        visited[i] = false;
+    }
+    visited[0] = true;
     // bfs
-    while (head != tail)
-    {
+    while (head != tail){
         node_x = queue[head].x;
         node_y = queue[head].y;
         if (node_x == end && node_y == end){
             break;
         }
         for(int i = 0; i < size; i++){
-            if(distance[node_y][i] == -1){
+            if(maze[node_y][i] == 1 && visited[i] == false){
+                ancient[i] = node_y;
                 //node_x = 0 0
                 queue[tail].x = node_y;//0 1
                 queue[tail].y = i;//1
-                distance[node_y][i] = distance[node_x][node_y] + 1;//00 01 12
+                visited[i] = true;
                 node_x = node_y;
                 node_y = i;
                 tail++;
@@ -64,31 +53,23 @@ int* BFS(int src, int end, int **maze, int size){
         //printf("node_x = %d, node_y = %d\n", node_x, node_y);
     }
     // generate route
-    int *route = malloc((1000000) * sizeof(int));
-    int now_x = end;
-    int now_y = end;
-    //printf("total step is : %d\n", distance[end][end]);
-    step = distance[end][end];
-    for (int i = step - 1; i >= 0; i--){
-        for(int j = 0; j < size; j++){
-            if(distance[j][now_x] == distance[now_x][now_y] - 1){
-                route[i] = now_x;
-                now_y = now_x;
-                now_x = j;
-                break;
-            }
-        }
+    int rroute[1000000];
+    int i = end, j = 0;
+    while(ancient[i] != src){
+        rroute[j] = i;
+        j++;
+        i = ancient[i];
+    }
+    int *route = malloc((j + 1) * sizeof(int));
+    for(int i = 0; i < j + 1; i++){
+        route[i] = rroute[j - i];
     }
     return route;//第0元素是起點
-    /*for (int i = 0; i < step>; i++){
-        printf("%d ", route[i]);
-    }
-    printf("\n");*/
 }
-tree* CBT(int front, int rear, int*route, int** Nodemem, tree *curNode, int Time){//目前是無窮迴圈狀態
+tree* CBT(int front, int rear, int*route, int** Nodemem, tree *curNode, int Time){
     int mid = (front + rear + 1) / 2;
     int TT = Time -  1;
-    int check;
+    int check = 0;
     tree *newnode = (tree *)malloc(sizeof(tree));
     newnode -> data_front = route[front];
     newnode -> data_rear = route[rear];
@@ -210,6 +191,7 @@ int main(){
     for(int i = 0; i < Links; i++){
         scanf("%d %d %d", &trash, &st, &ed);
         Linkmem[st][ed] = true;
+        Linkmem[ed][st] = true;
     }
     int Reqmem[Nodes][Nodes];
     for(int i = 0; i < Req; i++){
